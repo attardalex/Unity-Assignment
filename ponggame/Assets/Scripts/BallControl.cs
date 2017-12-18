8a74d9f92a9207681b4bacc7a4a709cd02cc669c
Alexander Attard <a100950@mcast.org.mt>
2017-12-18 09:32:10 +0100

    This is the Unity Game

---
+++ ponggame/Assets/Scripts/BallControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    //function that chooses random direction for the ball
+    // ball will then start to move
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    //initialize rb2d variable
+    //call GoBall()
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2); //2 second wait before ball starts moving
+    }
+
+    //ball is adjusted when hitting the paddle
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("paddle1"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (collision.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+
+        if (collision.collider.CompareTag("paddle2"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (collision.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+
+
+    void Update()
+    {
+        //when ball goes behind leftplayer
+        if (transform.position.x < -10f)
+        {
+            //give right player a point
+            scoreBoard.instance.GiverightPlayerPoint();
+
+            this.transform.position = new Vector3(0f, 0f, 0f);
+            GoBall();
+        }
+
+        //when ball goes behind rightplayer
+        if (transform.position.x > 10f)
+        {
+            //give left player a point
+            scoreBoard.instance.GiveleftPlayerPoint();
+
+            this.transform.position = new Vector3(0f, 0f, 0f);
+            GoBall();
+        }
+
+    }
+}
+
+
+
+
+
---
+++ ponggame/Assets/Scripts/scoreBoard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class scoreBoard : MonoBehaviour {
+
+    public static scoreBoard instance;
+
+    //creating variables for text objects
+    public Text leftPlayerScoretxt;
+    public Text rightPlayerScoretxt;
+
+    //int variables for the score method
+    public int leftplayerScore;
+    public int rightplayerScore;
+    public int maxPoints = 3;
+
+	
+	void Start () {
+        instance = this;
+
+        leftplayerScore = rightplayerScore = 0; //both player start with 0 as score
+	}
+	
+	void Update () {
+        if(leftplayerScore >= maxPoints || rightplayerScore >= maxPoints)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+	}
+
+    public void GiveleftPlayerPoint()
+    {
+        leftplayerScore += 1;
+
+        leftPlayerScoretxt.text = leftplayerScore.ToString();
+    }
+
+    public void GiverightPlayerPoint()
+    {
+        rightplayerScore += 1;
+
+        rightPlayerScoretxt.text = rightplayerScore.ToString();
+    }
+}
